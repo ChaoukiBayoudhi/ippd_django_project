67630abf723a4ef0e47b70bf12578c2acfb0ef90
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2026-02-23 17:40:32 +0100

    Register Product model

--- ippd_django_project/product_app/models.py
+++ ippd_django_project/product_app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.db.models import constraints, indexes
+from django.db.models.fields import uuid
 
 class Product(models.Model):
-    id=models.UUIDField(primary_key=True, editable=False)
+    id=models.UUIDField(primary_key=True,
+    default=uuid.uuid4,
+    editable=False)
     sku=models.CharField(max_length=70,unique=True)
     name=models.CharField(max_length=50)
     description=models.TextField()
@@ -11,6 +15,17 @@ class Product(models.Model):
     photo=models.ImageField(upload_to='images/products/',blank=True,null=True)
     manufactured_date=models.DateField(auto_now_add=True)
     expiry_date=models.DateField(auto_now=True)
+
+    class Meta:
+        #custom table name
+        db_table='products'
+        #introducing ordering by manufactured date in descending order
+        #and then by name in ascending order
+        ordering=['-manufactured_date','name']
+
+        indexes=[models.Index(fields=['name','category']),]
+        constraints=[models.UniqueConstraint(fields=['name','photo'],name="product_name_photo_unique")]
+
     
 
     
